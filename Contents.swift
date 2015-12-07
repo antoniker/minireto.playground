eebc8847fe800777d9c237f617e32945fb7cfa55
antoniker <jos_mustang@hotmail.com>
2015-12-07 16:18:31 -0600

    minireto

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//mini reto Alumno José Antonio Franco Cortés
+
+
+for s in 0...100 {
+    
+    if s >= 30 && s <= 40 {
+        print("\(s) Viva Swift")
+    }
+        
+    else
+    {
+    if s % 5 == 0{
+        print("\(s) Bingo")
+    }
+
+    if  s % 2 == 0 {
+        print("\(s) Es par")
+        
+    
+    }
+    else
+    {
+        print("\(s) Es impar")
+    }
+  }
+}
